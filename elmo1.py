a408d8de41d45e5812b3fec7e4e5b0d07bd546b0
Andrei <z@sp.sp>
2020-07-13 23:44:38 +0300

    1

--- elmo1.py
+++ elmo1.py
@@ -23,11 +23,24 @@ def elmo_vectors(x):
 #         "We use machine learning for text classification",
 #         "Text classification is subfield of machine learning"]
 
+"""
 corpus=["First name",
         "Second name",
          "Given name",
          "Last name",
          "Surname"]
+#[[1.0000004  0.861894   0.6049657  0.72111595 0.4558046 ]
+# [0.861894   1.         0.5898464  0.68965757 0.43921626]
+# [0.6049657  0.5898464  1.         0.60682935 0.41435632]
+# [0.72111595 0.68965757 0.60682935 1.         0.50063854]
+# [0.4558046  0.43921626 0.41435632 0.50063854 0.9999999 ]]
+"""
+
+corpus=["Click on the button below",
+        "Press this link and go",
+         "open website",
+         "navigate web page"]
+
 
 elmo_embeddings=[]
 print(len(corpus))
