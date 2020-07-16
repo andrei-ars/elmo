cc52175a2f541475463bba720bd042aef57d7ade
Andrei <z@sp.sp>
2020-07-16 19:49:17 +0300

    1

--- elmo_torch.py
+++ elmo_torch.py
@@ -43,9 +43,13 @@ def find_most_similar(text, texts):
     texts = ["long street", "the large city", "small button", "little long button"]
     """
     #corpus = [[t] for t in texts]
+    texts = [text] + texts
     corpus = [t.split() for t in texts]
-    sims = similarity_matrix(corpus)
-    return None
+    ext_sims = similarity_matrix(corpus)
+    print(ext_sims)
+    index = np.argmax(ext_sims[0,1:]) - 1
+    sim = ext_sims[0, index+1]
+    return index, sim
 
 
 
@@ -60,18 +64,21 @@ if __name__ == "__main__":
 
     texts = ["long street", "the large city", "small button", "little long button"]
     print(texts)
-    corpus = [text.split() for text in texts]
-    print(similarity_matrix(corpus))
     corpus = [[text] for text in texts]
     print(similarity_matrix(corpus))
-
+    corpus = [text.split() for text in texts]
+    print(similarity_matrix(corpus))
     """
     [[1.         0.75627625 0.66477364 0.7094424 ]
      [0.75627625 0.99999964 0.66632223 0.7353818 ]
      [0.66477364 0.66632223 1.0000001  0.8111696 ]
      [0.7094424  0.7353818  0.8111696  0.99999976]]
+    
     [[1.0000002  0.5136549  0.45660293 0.6065997 ]
      [0.5136549  1.         0.552847   0.4276201 ]
      [0.45660293 0.552847   0.9999996  0.71048474]
      [0.6065997  0.4276201  0.71048474 1.0000001 ]]
     """
+    text = "tiny label"
+    index, sim = find_most_similar(text, texts)
+    print("index={}, cos_similarity={}".format(index, sim))
