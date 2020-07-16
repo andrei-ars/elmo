b55a0bb8b87cb9833a589eef5c557b25eecbed6f
Andrei <z@sp.sp>
2020-07-16 20:25:29 +0300

    1

--- elmo_torch.py
+++ elmo_torch.py
@@ -94,6 +94,6 @@ if __name__ == "__main__":
     texts = ["simple button", "simple user", "just users", "site administrator", "user password", "open website"]
     sims = find_most_similar(text, texts)
 
-    text = "enter phrase"
+    text = "enter a phrase"
     texts = ["simple button", "input text", "site administrator", "user name", "open website"]
     sims = find_most_similar(text, texts)
